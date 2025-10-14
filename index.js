3eb5090c4c455fdcd3d49d35e6b2d0dcb22d6c48
abdlkader <abdlkadirdarwih@gmail.com>
2025-10-14 17:20:11 +0300

    Fix CORS for new frontend URL

--- index.js
+++ index.js
@@ -10,12 +10,12 @@ const bodyParser = require("body-parser");
 const PORT = process.env.PORT || 5000;
 
 const app = express();
-// app.use(cors());
-app.use(cors({
-  origin: "https://vercel-frontend-alpha-dun.vercel.app", // your frontend domain
-  methods: ["GET", "POST", "PUT", "DELETE"],
-  credentials: true
-}));
+app.use(cors());
+// app.use(cors({
+//   origin: "https://vercel-frontend-alpha-dun.vercel.app", // your frontend domain
+//   methods: ["GET", "POST", "PUT", "DELETE"],
+//   credentials: true
+// }));
 app.use(bodyParser.json());
  const URL = process.env.MONGODB_URL;
 
